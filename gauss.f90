4208f821ea07b9396ea1fefb4ec5d97a4b1c02da
admin <admin@pc-b139.local>
2017-09-29 16:39:48 +0900

    後退代入の簡単なverはできた

--- gauss.f90
+++ gauss.f90
@@ -2,15 +2,18 @@ program gauss
   !宣言部
   real,dimension(3,3) :: a = 0;
   real,dimension(3,1) :: b = 0;
-  real,dimension(1,3) :: x = 0;
+  real,dimension(3,1) :: x = 0;
   real,dimension(1,3) :: tmp = 0;
   real tmpb;
+  real sum;
   integer pivot;
   real coefficient;
+
 
   !実行部
   pivot = 0;
   tmpb = 0;
+  sum = 0;
   coefficient = 0;
   a(1,1) = 1; a(1,2) = 2;  a(1,3) = -2; b(1,1) = 3;
   a(2,1) = 1; a(2,2) = -1; a(2,3) = 3; b(2,1) = 4;
@@ -41,12 +44,6 @@ program gauss
       b(i,1) = b(pivot,1);
       b(pivot,1) = tmpb;
 
-      !行列の表示用
-      print *,"行交換"
-      do k = 1, 3
-        write(*, *)  a(k, 1:3),b(k,1)
-      enddo
-
       !消去するゾー
       do j=i+1,3
         coefficient = a(j,i)/a(i,i)
@@ -55,18 +52,27 @@ program gauss
 
       end do
 
-      !行列の表示用
-      print *,"割り算からの消去"
-      do k = 1, 3
-        write(*, *)  a(k, 1:3),b(k,1)
-      enddo
-
-
-    
-
-
     end do
 
+    print *,"整形結果"
+    do k = 1, 3
+      write(*, *)  a(k, 1:3),b(k,1)
+    enddo
+
+    do i = 3,1,-1
+      if(i == 3) then
+        x(i,1) = b(i,1)/a(i,i)
+      else if(i<3) then
+        do j=i,3
+          sum = sum + a(i,j)*x(j,1)
+        end do
+        x(i,1) = (b(i,1) - sum)/a(i,i)
+      end if
+      sum = 0
+    end do
+
+    print *,"最終結果"
+    write(*, *)  x(1:3,1)
 
 
 end program gauss
